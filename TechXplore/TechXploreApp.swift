e8001a88159aa3f848c9f66df5d4bcf729325775
likanozadze <145837358+likanozadze@users.noreply.github.com>
2024-07-05 16:58:54 +0400

    first commit

---
+++ TechXplore/TechXploreApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechXploreApp.swift
+//  TechXplore
+//
+//  Created by Lika Nozadze on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct TechXploreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
